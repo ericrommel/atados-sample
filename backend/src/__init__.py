89927fdd6010f02b5549331864fc3c2414675fac
Eric Dantas <ericrommel@gmail.com>
2020-09-11 22:18:53 +0200

    Start Flask basic app. Set config files

---
+++ backend/src/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SECRET_KEY="TEMPORARY",
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def hello():
+        return "Hello, World!"
+
+    return app
